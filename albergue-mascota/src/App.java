fa72ff946fc65652df54ab289cdd11e51492eb14
Jhoel <escatejhoel@gmail.com>
2026-09-09 22:12:14 -0500

    Implementando el menu de navegacion

--- albergue-mascota/src/App.java
+++ albergue-mascota/src/App.java
@@ -1,23 +1,35 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Mascota mascota_1 = new Mascota("Pastor Aleman", 2, "Macho", 35.7, 1.05);
-        Mascota mascota_2 = new Mascota("Pitbull", 3, "Hembra", 25, 50);
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0;
 
-        IO.println("============REGISTRO DE MASCOTA RESCATADA - PERRO============");
-        IO.println("Raza: "+ mascota_1.getRaza());
-        IO.println("Edad: " + mascota_1.getEdad() + " años");
-        IO.println("Genero: " + mascota_1.getGenero());
-        IO.println("Peso: " + mascota_1.getPeso() + " kg");
-        IO.println("Tamaño: " + mascota_1.getTamaño() + " m");
-        IO.println("==============================================================\n");
-        IO.println("============REGISTRO DE MASCOTA RESCATADA - PERRO============");
-        IO.println("Raza: "+ mascota_2.getRaza());
-        IO.println("Edad: " + mascota_2.getEdad() + " años");
-        IO.println("Genero: " + mascota_2.getGenero());
-        IO.println("Peso: " + mascota_2.getPeso() + " kg");
-        IO.println("Tamaño: " + mascota_2.getTamaño() + " m");
-        IO.println("==============================================================");
+        do {
+            System.out.println("\n--- SISTEMA ALBERGUE MASCOTAS ---");
+            System.out.println("1. Registrar Mascota");
+            System.out.println("2. Ver Datos de Mascota");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opción: ");
 
+            opcion = scanner.nextInt();
+            scanner.nextLine(); 
 
+            switch (opcion) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        } while (opcion != 3);
+
+        scanner.close();
     }
 }
